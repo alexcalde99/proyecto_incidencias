5167d91e3aaf2ead0453d14c1b176794274b1617
Feram <feram@feram.co>
2016-06-12 22:09:20 +0000

    Fix typos

--- htdocs/proyecto_incidencias/assets/grocery_crud/texteditor/ckeditor/ckeditor_php5.php
+++ htdocs/proyecto_incidencias/assets/grocery_crud/texteditor/ckeditor/ckeditor_php5.php
@@ -43,7 +43,7 @@ class CKEditor
 	 * Example usage:
 	 * @code
 	 * $CKEditor->config['height'] = 400;
-	 * // Use @@ at the beggining of a string to ouput it without surrounding quotes.
+	 * // Use @@ at the beginning of a string to ouput it without surrounding quotes.
 	 * $CKEditor->config['width'] = '@@screen.width * 0.8';
 	 * @endcode
 	 */
